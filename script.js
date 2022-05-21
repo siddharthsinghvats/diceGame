a6ba6764881717880582598da965bf01e17d3455
Siddharth Singh <95075464+siddharthsinghvats@users.noreply.github.com>
2022-05-21 19:33:38 +0530

    add delay in title update

--- script.js
+++ script.js
@@ -4,19 +4,25 @@
 document.getElementById("play").addEventListener('click',function(){
     let x = Math.ceil(Math.random()*6);
     let y = Math.ceil(Math.random()*6);
-    console.log(x);
     document.getElementById("p1").setAttribute('src',`${x}.png`);
     document.getElementById("p2").setAttribute('src',`${y}.png`);
     const elem = document.getElementById("heading");
     if(x>y){
-        elem.textContent="Player 1 Won!!";
+        setTimeout(function(){
+            elem.textContent="Player 1 Won!!";
+        },100);
+        
        
     }
     else if(x<y){
-        elem.textContent="Player 2 Won!!"; 
+        e setTimeout(function(){
+            elem.textContent="Player 2 Won!!";
+        },100);
       
     }
     else{
-        elem.textContent="Draw!!";
+        setTimeout(function(){
+            elem.textContent="Draw!!";
+        },100);
     }
 })
